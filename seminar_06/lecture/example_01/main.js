93011d3bdd2474e5efde33228c1283bfaea16ca2
akorzun <akorzun@exadel.com>
2017-05-12 14:38:50 +0300

    added seminar 06

--- seminar_06/lecture/example_01/main.js
+++
@@ -1,9 +0,0 @@
-function DiaryDay() {
-    var args = [].slice.call(arguments);
-    this.dayNumber = args[0] || 1;
-    this.tasks = [];
-}
-window.onload = function () {
-    var day = new DiaryDay();
-    
-};
