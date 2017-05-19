5b763f86d885bae0acee2c718b4bcf707f9b7a45
akorzun <akorzun@exadel.com>
2017-05-19 17:25:53 +0300

    added initial structure of to-do list that was implemented previously

--- seminar_08/lecture/initial-to-do-list/js/list.js
+++ seminar_08/lecture/initial-to-do-list/js/list.js
@@ -1,22 +1,28 @@
+/**
+ * TO-DO List
+ * @param {string} selector
+ * @constructor
+ */
 function List(selector) {
     this.el = document.querySelector(selector);
-    this.helper = new Modal('.modal.add-task').setupEvents();
+    this.helper = new Modal('.modal.add-task');
     this.tasks = [];
 }
+
+/**
+ * Set event listeners
+ */
 List.prototype.setupEvents = function () {
     this.el.addEventListener('click', this.clickHandler.bind(this));
-    this.el.addEventListener('addTask', this.addTaskHandler.bind(this));
 };
 
+/**
+ * Click handler on List container
+ * @param {Event} e
+ */
 List.prototype.clickHandler = function (e) {
     var targetClasses = e.target.className.split(' ');
     if (targetClasses.indexOf('show-modal') !== -1) {
         this.helper.show();
-    }
-};
-
-List.prototype.addTaskHandler = function (e) {
-    if (e.detail.message) {
-        //create Task
     }
 };
--- seminar_08/lecture/initial-to-do-list/js/modal.js
+++ seminar_08/lecture/initial-to-do-list/js/modal.js
@@ -1,40 +1,43 @@
+/**
+ * Modal window
+ * @param {string} selector
+ * @constructor
+ */
 function Modal(selector) {
     this.el = document.querySelector(selector);
+    
+    this.setupEvents();
 }
 
+/**
+ * Set event listeners
+ */
 Modal.prototype.setupEvents = function () {
     this.el.addEventListener('click', this.clickHandler.bind(this));
-    return this;
 };
 
+/**
+ * Click handler on Modal container
+ * @param {Event} e
+ */
 Modal.prototype.clickHandler = function (e) {
     var targetClasses = e.target.className.split(' ');
     if (targetClasses.indexOf('delete') !== -1 ||
         targetClasses.indexOf('cancel') !== -1) {
         this.hide();
     }
-    if (targetClasses.indexOf('add-task') !== -1) {
-        var data = this.getData();
-        var eventAddTask = new CustomEvent('addTask', {
-            detail: data,
-            bubbles: true
-        });
-        this.el.dispatchEvent(eventAddTask);
-        this.hide();
-    }
 };
 
+/**
+ * Show modal window
+ */
 Modal.prototype.show = function () {
     this.el.classList.add('is-active');
 };
 
+/**
+ * Hide modal window
+ */
 Modal.prototype.hide = function () {
     this.el.classList.remove('is-active');
-    this.el.querySelector('textarea').value = '';
-};
-
-Modal.prototype.getData = function () {
-    var data = {};
-    data.message = this.el.querySelector('textarea').value || null;
-    return data;
 };
